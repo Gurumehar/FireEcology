51da018ce2d5ef75465ffac814b3b2b05c6ecc38
Gurumehar <81568195+Gurumehar@users.noreply.github.com>
2023-07-19 19:32:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+background_music=document.getElementById("C:\Users\Public\Music\Sample Music");
+function play_background() {
+background_music.play();
+}
+video=document.getElementById("https://www.youtube.com/watch?v=W_ntvTEben4&t=392s&pp=ygUMZmlyZSBlY29sb2d5");
+function video() {
+video.play();
+}
